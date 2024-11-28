3c4fa641f8af17bc1b47e7409cf846aee9100f1e
Aryan shukla <113294396+Aryanshukla206@users.noreply.github.com>
2024-11-28 11:29:54 -0800

    Update AnalyticsPage.jsx

--- src/Components/AnalyticsPage.jsx
+++ src/Components/AnalyticsPage.jsx
@@ -5,9 +5,9 @@ const AnalyticsPage = () => {
       <h1>Analytics Dashboard</h1>
       
       <p>This is where your analytics charts and graphs would go.</p>
-      <img src={"https://media.istockphoto.com/id/1929916076/photo/work-in-progress.jpg?s=612x612&w=0&k=20&c=2hD7jju8ev7uYEakRZZanOfFUIabxOQgZHqnJovQ6Cs="} 
-         alt="Under Construction" className="max-h-64 rounded-lg shadow-lg" />
+      <p>This Page is visible to Manager and Admin</p>
     </div>
+    
     
 
   );
