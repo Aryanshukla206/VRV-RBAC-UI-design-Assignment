3a53becb003be696881ad57fa9c7001eea20293d
Aryan shukla <113294396+Aryanshukla206@users.noreply.github.com>
2024-11-28 11:29:15 -0800

    Update TicketsPage.jsx

--- src/Components/TicketsPage.jsx
+++ src/Components/TicketsPage.jsx
@@ -3,8 +3,7 @@ const TicketsPage = () => {
     <div className="member-card flex flex-col items-center justify-center p-6">
       <h1>Ticket Management</h1>
       <p>This is where you would manage tickets.</p>
-      <img src={"https://media.istockphoto.com/id/1929916076/photo/work-in-progress.jpg?s=612x612&w=0&k=20&c=2hD7jju8ev7uYEakRZZanOfFUIabxOQgZHqnJovQ6Cs="} 
-         alt="Under Construction" className="max-h-64 rounded-lg shadow-lg" />
+      <p>This Page is visible to Support, Manager and Admin</p>
     </div>
   );
 };
